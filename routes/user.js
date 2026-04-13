7e2052f366b78ecc69491a0ecf25dc2ef7cd2d4a
Gabriel Furlaneto <gabrielfulanetoneves@gmail.com>
2026-04-13 08:15:08 -0300

    nhdagj

--- routes/user.js
+++ routes/user.js
@@ -91,4 +91,30 @@ router.get('/:id', (req, res) => {
     });
 });
 
+app.get('/melhor-volta', (req, res) => {
+    db.query(
+      `SELECT c.nome, MIN(v.tempo) AS melhor_volta
+       FROM voltas v
+       JOIN corredores c ON v.corredor_id = c.id`,
+      (err, result) => {
+        if (err) return res.status(500).json(err);
+        res.json(result[0]);
+      }
+    );
+  });
+
+
+  router.get('/tempo-total', (req, res) => {
+    db.query(
+      `SELECT c.nome, SUM(v.tempo) AS tempo_total
+       FROM voltas v
+       JOIN corredores c ON v.corredor_id = c.id
+       GROUP BY c.id`,
+      (err, result) => {
+        if (err) return res.status(500).json(err);
+        res.json(result);
+      }
+    );
+  });
+
 module.exports = router;
